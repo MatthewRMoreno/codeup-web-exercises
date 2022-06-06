0407d6c76789d63f2022621cd719aac6ecce3290
Matthew Moreno <matthew.r.moreno21@gmail.com>
2022-06-06 17:12:56 -0500

    Fixed codeup-web-exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,6 +1,21 @@
 "use strict";
-function showMultiplicationTable(number, x) {
-    for(let x = 1; x <= 10; x++){
-
+function showMultiplicationTable(number) {
+    for(let i = 1; i <= 10; i++){
+        console.log((number) + " x " + (i) + " = " + (number * i))
     }
 }
+
+function randomOddOrEven(x) {
+    for (let i = 0; i >= 20, i <= 200; i++) {
+        let random = Math.floor((Math.random() * 180) + 20)
+        if (random % 2 === 0) {
+            console.log("The number " + random + " here is even.");
+        }else{
+            console.log("The number " + random + " here is odd.");
+        }
+    }
+}
+
+// for (let i = 1; i < 10, i++;) {
+//
+// }
