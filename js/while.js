04e7c767ecd839a86f4dfe143299b96ac02dca99
Matthew Moreno <matthew.r.moreno21@gmail.com>
2022-06-07 11:50:51 -0500

    Complete:Finished Loops exercises in JS

---
+++ js/while.js
@@ -0,0 +1,18 @@
+// let i = 2;
+// while(i <= 65536){
+//     console.log(i);
+//     i *= 2
+// }
+function iceCreamForSale() {
+    let conesForSale = Math.floor(Math.random() * 50) + 50;
+    while (conesForSale > 0) {
+        let conesBought = Math.floor(Math.random() *5) + 1;
+        if (conesBought > conesForSale) {
+            console.log("Cannot sell you " + conesBought + " cones I only have " + conesForSale + "...");
+        }
+            conesForSale = conesForSale - conesBought;
+            console.log(conesBought + " cones sold ...");
+    }
+        console.log("Yay! I sold them all!");
+}
+iceCreamForSale()
