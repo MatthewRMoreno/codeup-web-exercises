b316383e1c4e736b914b9329199a357b42391184
Matthew Moreno <matthew.r.moreno21@gmail.com>
2022-06-08 09:47:56 -0500

    Updated code for randomOddOrEven

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,13 +1,13 @@
 "use strict";
 function showMultiplicationTable(number) {
     for(let i = 1; i <= 10; i++){
-        console.log((number) + " x " + (i) + " = " + (number * i))
+        console.log((number) + " x " + (i) + " = " + (number * i));
     }
 }
 
 function randomOddOrEven(x) {
-    for (let i = 0; i >= 20, i <= 200; i++) {
-        let random = Math.floor((Math.random() * 180) + 20)
+    for (let i = 0; i < 10; i++) {
+        let random = Math.floor((Math.random() * 180) + 20);
         if (random % 2 === 0) {
             console.log("The number " + random + " here is even.");
         }else{
@@ -18,7 +18,7 @@ function randomOddOrEven(x) {
 
 function plusSame(x) {
     for (let i = 1; i < 10; i++) {
-        var count = []
+        var count = [];
         for (let b = 1; b<= i; b++) {
             count.push(i);
         }
