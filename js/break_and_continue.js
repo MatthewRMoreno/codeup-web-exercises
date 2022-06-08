6dec26fad8d0d5d40a6c48a9fcf710c65236a9b8
Matthew Moreno <matthew.r.moreno21@gmail.com>
2022-06-08 10:11:05 -0500

    Added new code into break and continue JS

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,5 +1,13 @@
 "use strict"
+//prompt user to enter a number
 let question = prompt("Hello user would you please enter a odd number between 1 and 50.")
+while(true) {
+    if (question % 2 !== 0 && question >= 1 && question <= 50) {
+        console.log("Odd number entered please continue.");
+        break;
+    }
+    question = prompt("Sorry, that is not an odd number. Please enter an odd number between 1 and 50.")
+}
 //switches the answer to the prompt a number data type
 let value = parseInt(question)
 //Rounds he nuber up or down if it is a decimal value
