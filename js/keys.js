18a1f74283de2ae054507e1db5c8d799cf69d343
Matthew Moreno <matthew.r.moreno21@gmail.com>
2022-07-22 09:13:04 -0500

    added a new key

--- js/keys.js
+++ js/keys.js
@@ -1,2 +1,3 @@
 // my api keys go here
 const MAP_BOX_API_KEY = "pk.eyJ1IjoibWF0dG1vcmVubyIsImEiOiJjbDVsNGFvcTIwMGNyM2RvZG9vM2k0ZmdlIn0.ltJrWt9lIC6afF9YiyjYzg";
+const OPEN_WEATHER_API_KEY = "1829c144067443ff844de3686e10d5aa"
